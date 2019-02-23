c7efcaa51bb2e725accba69bc2794c53643765ae
devcomb <james@devcomb.com>
2019-02-23 04:50:40 +0000

    fixup

--- nginx-client/nginx-start/convertENV.sh
+++ nginx-client/nginx-start/convertENV.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 export OAUTH_AUTH_SERVER=$(curl https://${KUBERNETES_SERVICE_HOST}/.well-known/oauth-authorization-server -ks|python -c "import sys, json; print(json.load(sys.stdin)['issuer'])") 
+echo "OAUTH_AUTH_SERVER=${OAUTH_AUTH_SERVER}"
 export OS_CONSOLE_HOST="$(python -c "from urlparse import urlparse;print(urlparse('https://console.devcomb.com:8443').hostname)")"
 export OS_CONSOLE_PORT="$(python -c "from urlparse import urlparse;print(urlparse('https://console.devcomb.com:8443').port)")"
 export NAMESERVER=$(echo $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) )
