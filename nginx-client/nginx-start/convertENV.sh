ac1bc357a799e4dc52ca0ee80a74a8819bf96d90
devcomb <james@devcomb.com>
2019-02-26 18:35:23 +0000

    FIXUP

--- nginx-client/nginx-start/convertENV.sh
+++ nginx-client/nginx-start/convertENV.sh
@@ -5,7 +5,7 @@ export OS_CONSOLE_HOST="$(python -c "from urlparse import urlparse;print(urlpars
 export OS_CONSOLE_PORT="$(python -c "from urlparse import urlparse;print(urlparse('${OAUTH_AUTH_SERVER}').port)")"
 export NAMESERVER=$(echo $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) )
 export NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-envsubst '${APPWEB1_ADDR},${APPWEB2_ADDR},${APPWEB3_ADDR}'< ${NGINX_CONFIGURATION_PATH}/upstream.conf > ${NGINX_CONFIGURATION_PATH}/upstream.conf
+envsubst '${WEBAPP_ADDR},${WEBAPP_DEV_ADDR},${IDE_ADDR}'< ${NGINX_CONFIGURATION_PATH}/upstream.conf > ${NGINX_CONFIGURATION_PATH}/upstream.conf
 envsubst '${AUTH_HOST},${REMOTE_WEBAPP_HOST},${GROUP_AUTH_HOST},${NAMESPACE},${SA_TOKEN},${OS_CONSOLE_HOST},${OS_CONSOLE_PORT},${GROUP_REQ}' < ${NGINX_CONFIGURATION_PATH}/subdomains.conf > ${NGINX_CONFIGURATION_PATH}/subdomains.conf
 echo 1 > /tmp/health
 echo "Finished convertENV.sh"
